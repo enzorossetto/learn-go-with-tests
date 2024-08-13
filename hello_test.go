fee784ee1489d8a61c045d78a0c95ec557688e44
Enzo Rossetto <enzojmrossetto@gmail.com>
2024-08-12 21:18:18 -0300

    feat: implement subtests

--- hello_test.go
+++ hello_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	t.Run("saying hello to people", func(t *testing.T) {
+	t.Run("Saying hello to people", func(t *testing.T) {
 		got := Hello("Chris")
 		want := "Hello, Chris"
 	
@@ -12,7 +12,7 @@ func TestHello(t *testing.T) {
 		}
 	})
 
-	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+	t.Run("Say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, World"
 
